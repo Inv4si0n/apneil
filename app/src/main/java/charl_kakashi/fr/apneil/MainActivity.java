a1b22ce342da3b99d37558ecffe3f882e9144a6a
Alves <38011810+Inv4si0n@users.noreply.github.com>
2018-04-05 15:34:05 +0200

    Update MainActivity.java

--- app/src/main/java/charl_kakashi/fr/apneil/MainActivity.java
+++ app/src/main/java/charl_kakashi/fr/apneil/MainActivity.java
@@ -1,11 +1,13 @@
 package charl_kakashi.fr.apneil;
 
+import android.bluetooth.BluetoothAdapter;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -21,18 +23,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null)
+            Toast.makeText(MainActivity.this, "Pas de Bluetooth",
+                    Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(MainActivity.this, "Avec Bluetooth",
+                    Toast.LENGTH_SHORT).show();
+
         GraphView graph = findViewById(R.id.graph);
 
-        graph.setTitle("Titre"); // Titre du graph
+        graph.setTitle("Apnée par nuit"); // Titre du graph
         graph.setTitleColor(Color.BLUE); // Couleur du titre du graph
 
         series = new LineGraphSeries<>(generateData());
 
-        series.setColor(Color.GREEN); // Couleur de la courbe
+        series.setColor(Color.RED); // Couleur de la courbe
         series.setDrawDataPoints(true); // Tracé les points
         series.setDataPointsRadius(10); // Radius points
         series.setThickness(2); // Epaisseur
@@ -40,11 +49,7 @@ public class MainActivity extends AppCompatActivity {
         graph.addSeries(series);
 
         Button button = findViewById(R.id.on);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("ta mere la chauve");
-            }
-        });
+        Button buttona = findViewById(R.id.off);
     }
 
 
@@ -55,10 +60,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 series.resetData(generateData());
-                handler.postDelayed(this, 3000);
+                handler.postDelayed(this, 1500);
             }
         };
-        handler.postDelayed(timer, 3000);
+        handler.postDelayed(timer, 1500);
 
     }
 
@@ -76,11 +81,13 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < count; i++) {
             double x = i;
             double f = rand.nextDouble() * 0.15 + 0.3;
-            double y = Math.sin(i * f + 2) + rand.nextDouble() * 0.3;
+            double y = rand.nextInt();
             DataPoint v = new DataPoint(x, y);
             values[i] = v;
         }
         return values;
     }
+
+
 
 }
